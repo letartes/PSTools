30c312a3d68e52ee8a2e6d20a2c9721ffcabff90
letartes <sletarte@nonagon-2.local>
2013-11-02 15:12:29 -0700

    removed rMarkdown and put a r executable example instead

---
+++ doc/Usage_plot_heatmap_peptides.R
@@ -0,0 +1,11 @@
+# Load some data
+setwd("~/Dropbox/code/git/PSTools")
+peptides.df <- read.csv("data/enolase_runs.csv")
+fasta <- read.table("data/eno1.fasta", sep="\t", header=TRUE)
+eno <- as.character(fasta[1,1])
+
+# Load the function and run it
+source("code/plot_heatmap_peptides.R")
+p <- plot.heatmap.peptide(peptides.df, eno)
+p
+
